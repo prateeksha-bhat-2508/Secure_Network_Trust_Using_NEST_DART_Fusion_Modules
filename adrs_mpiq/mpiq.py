a05555aa67e56ead31b4d99eb5bbfa1dc7d1baf7
Prateeksha Bhat <prateebhat2004@gmail.com>
2026-07-04 19:16:22 +0530

    ADRS accuracy increased

--- adrs_mpiq/mpiq.py
+++ adrs_mpiq/mpiq.py
@@ -5,16 +5,10 @@ class MPIQ:
 
     def compute(self):
 
-        self.df["ADRS_MPIQ_Score"]=(
-
-            0.4*self.df["Fitness"]
-
-            +0.3*self.df["Routing_Score"]
-
-            +0.2*self.df["Hybrid_Trust"]
-
-            +0.1*self.df["BARM_Score"]
-
-        ).clip(0,1)
-
+        self.df["ADRS_MPIQ_Score"] = (
+    0.3 * self.df["BARM_Score"]
+    + 0.3 * self.df["Hybrid_Trust"]
+    + 0.2 * self.df["Centrality_Trust"]
+    + 0.2 * (1 - self.df["Attack_Probability"])
+).clip(0, 1)
         return self.df
